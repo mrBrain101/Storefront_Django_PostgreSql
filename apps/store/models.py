3e6b51911e23a4b1f411883c279d6c645a9d0d39
Sergey K <sergey.kuroptev@gmail.com>
2025-10-24 12:01:41 +0300

    feat(carts): Add cart and cart items CRUD functionality, minor updates.

--- apps/store/models.py
+++ apps/store/models.py
@@ -118,6 +118,9 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        unique_together = [['cart', 'product']]
